fb79e44255e19baea41ea1c801a22629d3715a7d
eparkko-lab <erik.parkkonen@yubico.com>
2020-01-02 14:24:23 -0800

    copy to new repo

---
+++ include/testapi.js
@@ -0,0 +1,214 @@
+var suiteDetails;
+
+function getSuite(suiteName) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("testsuiteDetails").textContent = JSON.stringify(JSON.parse(this.responseText),null,2);
+      suiteDetails = JSON.parse(this.responseText);
+      document.getElementById("totalTestCount").textContent = suiteDetails.tests.length;
+    }
+  };
+  xhttp.open("GET", "https://xhbg5kpuu7.execute-api.us-east-2.amazonaws.com/p/suite/" + suiteName + "?client_id=yaN8bv3EOemBtWNVPEryZO67U0OFJ14l4DNEI640", true);
+  xhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
+  xhttp.setRequestHeader("Accept", "application/json");
+  xhttp.send();
+}
+
+function executeTests() {
+
+  //suiteDetails=JSON.parse('{"tests":[{"sequence":"3","testId":"t1"},{"sequence":"1","testId":"t2"},{"sequence":"2","testId":"t3"},{"sequence":"12","testId":"t3"}],"suiteName":"defaultValuesGetAndCreate"}');    
+  sessionId = document.getElementById("sessionId")
+  if ( sessionId.textContent ){
+    //do nothing
+  } else {    
+    sessionId.textContent = uuidv4();
+    console.log("sessionId:" + sessionId.textContent);
+  }
+
+
+  var tests = suiteDetails.tests;
+  var orderedTests = tests.sort((a, b) => (parseInt(a.sequence) > parseInt(b.sequence)) ? 1 : -1);
+  var arrayLength = orderedTests.length;
+
+  const loopTests = async n => {
+
+    for (var i = 0; i < n; i++) {
+      console.log("processing test #:" + (i + 1) + " of: " + n);
+      document.getElementById("currentTestCount").textContent = (i + 1);
+
+      await processTest(orderedTests[i]);
+    }
+    return (console.log("complete"))
+  }
+  loopTests(arrayLength);
+}
+
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+var processTest = function (test) {
+  return new Promise((resolve, reject) => {
+    var testDetails;
+    var requestDetails;
+
+    var sleep = function (ms) {
+      return new Promise((resolve, reject) => {
+        console.log("start sleep: " + ms + "ms");
+        setTimeout(resolve, ms);
+      }
+      )
+    }
+
+    var getRequest = function (testDetails) {
+      return new Promise((resolve, reject) => {
+        console.log("testDetails:" + testDetails);
+        var req3 = new XMLHttpRequest();
+
+        req3.open("GET", "https://xhbg5kpuu7.execute-api.us-east-2.amazonaws.com/p/request/" + testDetails.requestId + "?client_id=yaN8bv3EOemBtWNVPEryZO67U0OFJ14l4DNEI640", true);
+        req3.setRequestHeader("Access-Control-Allow-Origin", "*");
+        req3.setRequestHeader("Accept", "application/json");
+        req3.setRequestHeader("Content-Type", "application/json");
+        req3.addEventListener("load", function () {
+          if (req3.status < 400) {
+
+            requestDetails = JSON.parse(this.responseText);
+            //console.log("requestDetails: " + requestDetails.requestType);
+            resolve(requestDetails)
+          } else {
+            reject(new Error("Request failed: " + req3.statusText));
+          }
+        });
+        req3.addEventListener("error", function () {
+          reject(new Error("Network error"));
+        });
+        req3.send();
+
+      })
+    }
+
+    var retrieveTest = function (test) {
+      return new Promise((resolve, reject) => {
+        var req2 = new XMLHttpRequest();
+        req2.open("GET", "https://xhbg5kpuu7.execute-api.us-east-2.amazonaws.com/p/test/" + test.testId + "?client_id=yaN8bv3EOemBtWNVPEryZO67U0OFJ14l4DNEI640", true);
+        req2.setRequestHeader("Access-Control-Allow-Origin", "*");
+        req2.setRequestHeader("Accept", "application/json");
+        req2.setRequestHeader("Content-Type", "application/json");
+        req2.addEventListener("load", function () {
+          if (req2.status < 400) {
+            document.getElementById("testDetails").textContent = JSON.stringify(JSON.parse(this.responseText),null,2);
+            testDetails = JSON.parse(this.responseText);
+            console.log("test requestId: " + testDetails.requestId);
+            resolve(testDetails);
+          } else {
+            reject(new Error("Request failed: " + req2.statusText));
+          }
+        });
+        req2.addEventListener("error", function () {
+          reject(new Error("Network error"));
+        });
+        req2.send();
+
+
+
+      })
+    }
+
+    var buildWebAuthnRequest = function (requestDetails) {
+      return new Promise((resolve, reject) => {
+        decodedWebauthnRequest = atob(requestDetails.requestBase64);
+        requestDetails["decodedWebauthnRequest"] = decodedWebauthnRequest;
+        requestDetails["populatedWebAuthnRequest"] = decodedWebauthnRequest;
+        //console.log("navigator.credentials." + requestDetails.requestType + "(" + requestDetails.requestBase64 + ")");  
+        document.getElementById("webauthnRequestType").textContent = requestDetails.requestType;
+        document.getElementById("webauthnRequest").textContent = JSON.stringify(JSON.parse(decodedWebauthnRequest),null,2);
+        resolve(requestDetails);
+
+      })
+    }
+
+    var executeWebAuthn = function (requestDetails) {
+      return new Promise((resolve, reject) => {
+        if (requestDetails.requestType === "get") {
+          authenticate();
+          resolve()
+        } else if (requestDetails.requestType === "create") {
+          register();
+          resolve();
+        } else {
+          reject();
+        }
+        //console.log("navigator.credentials." + requestDetails.requestType + "(" + requestDetails.decodedWebauthnRequest + ")");                
+      })
+    }
+
+    var postResult = function () {
+      return new Promise((resolve, reject) => {
+        var webAuthnResponse = btoa(document.getElementById("webauthnResponse").textContent);
+
+        var req4 = new XMLHttpRequest();
+        req4.open("POST", "https://xhbg5kpuu7.execute-api.us-east-2.amazonaws.com/p/result/?client_id=yaN8bv3EOemBtWNVPEryZO67U0OFJ14l4DNEI640", true);
+        req4.setRequestHeader("Access-Control-Allow-Origin", "*");
+        req4.setRequestHeader("Accept", "application/json");
+        req4.setRequestHeader("Content-Type", "application/json");
+        req4.addEventListener("load", function () {
+          if (req4.status < 400) {            
+            testDetails = JSON.parse(this.responseText);
+            console.log("posted results");
+            resolve(testDetails);
+          } else {
+            reject(new Error("Request failed: " + req4.statusText));
+          }
+        });
+        req4.addEventListener("error", function () {
+          reject(new Error("Network error"));
+        });
+
+        result = {
+          sessionId: document.getElementById("sessionId").textContent,
+          webauthnRequest: btoa(requestDetails["populatedWebAuthnRequest"]),
+          webauthnResponse: webAuthnResponse,
+          userAgent: navigator.userAgent,
+          suiteName: suiteDetails.suiteName,
+          testId: testDetails.testId,
+          resultId: ""
+        }
+        console.log("post: " + JSON.stringify(result));
+        req4.send(JSON.stringify(result));
+
+      }
+      )
+    }
+
+    retrieveTest(test)
+      .then(function (testDetails) {
+        return getRequest(testDetails);
+      })
+      .then(function (requestDetails) {
+        return buildWebAuthnRequest(requestDetails);
+      })
+      .then(function (requestDetails) {
+        return executeWebAuthn(requestDetails);
+      })
+      .then(function () {
+        return sleep(8000);
+      })
+      .then(function () {
+        return postResult();
+      })
+      .then(function () {
+        resolve("done");
+      })
+    //.then(sleep(100))      
+
+  })
+}
+
+
+
+
+
---
+++ include/webauthn.js
@@ -0,0 +1,218 @@
+function replaceMarkers(request) {
+  //request = JSON.parse(request);
+  var builtRequest = JSON.parse(request);
+
+  if (builtRequest.allowCredentials &&
+    builtRequest.allowCredentials[0] &&
+    builtRequest.allowCredentials[0].id === "${lastGeneratedId}") {
+    builtRequest.allowCredentials[0].id = localStorage.getItem('registeredCredentialId');
+  }
+
+  if (builtRequest.excludeCredentials &&
+    builtRequest.excludeCredentials[0] &&
+    builtRequest.excludeCredentials[0].id === "${lastGeneratedId}") {
+    builtRequest.excludeCredentials[0].id = localStorage.getItem('registeredCredentialId');
+  }
+  return builtRequest;
+}
+
+function getWebAuthnRequest(builderArea) {
+  var Request = document.getElementById(builderArea).value;
+  return JSON.parse(Request);
+}
+
+function authenticate() {
+  webAuthnRequest = getWebAuthnRequest("webauthnRequest");
+  webAuthnRequest = replaceMarkers(JSON.stringify(webAuthnRequest));
+
+  var webAuthnResponseDiv = document.getElementById("webauthnResponse");
+
+  getAssertion(webAuthnRequest)
+    .then(function (getAssertionResponse) {
+      console.log(getAssertionResponse);
+      webAuthnResponseDiv.textContent = JSON.stringify(responseToObject(getAssertionResponse), null, 2);
+    })
+    .catch(error => {
+      webAuthnResponseDiv.textContent = error;
+      throw error;
+    })
+}
+
+function getAssertion(webAuthnRequest) {
+  console.log('Get assertion', webAuthnRequest);
+  return navigator.credentials.get({
+    publicKey: decodePublicKeyCredentialRequestOptions(webAuthnRequest),
+  });
+}
+
+
+function decodePublicKeyCredentialRequestOptions(webAuthnRequest) {
+  //allowCredentials = webAuthnRequest.allowCredentials;
+  const allowCredentials = webAuthnRequest.allowCredentials && webAuthnRequest.allowCredentials.map(credential => extend(
+    credential, {
+    id: toByteArray(credential.id),
+  }));
+
+  const publicKeyCredentialRequestOptions = extend(
+    webAuthnRequest, {
+    allowCredentials,
+    challenge: toByteArray(webAuthnRequest.challenge),
+  });
+  console.log(publicKeyCredentialRequestOptions);
+  var webAuthnRequestDiv = document.getElementById("webauthGetRequest");
+
+  //webAuthnRequestDiv.textContent = JSON.stringify(publicKeyCredentialRequestOptions);
+
+  return publicKeyCredentialRequestOptions;
+}
+
+//-------------------------------------------------------------------
+//-------------------------------------------------------------------
+function register() {
+  webAuthnRequest = getWebAuthnRequest("webauthnRequest");
+  webAuthnRequest = replaceMarkers(JSON.stringify(webAuthnRequest));
+
+  var webAuthnResponseDiv = document.getElementById("webauthnResponse");
+
+  createCredential(webAuthnRequest)
+    .then(function (createCredentialResponse) {
+      console.log("createCredential:" + createCredentialResponse);
+      //console.log(JSON.stringify(createCredentialResponse.response.attestationObject));
+      //decodedAttestation = decodeAttestationObj(createCredentialResponse.response.attestationObject);
+      //createCredentialResponse.response.attestationObject = JSON.parse(decodedAttestation);
+      webAuthnResponseDiv.textContent = JSON.stringify(responseToObject(createCredentialResponse), null, 2);
+      localStorage.setItem('registeredCredentialId', createCredentialResponse.id)
+      console.log(createCredentialResponse.id)
+      //addCredentialIdToOptions("excludeCredentialsIdCreateMenu", createCredentialResponse.id)
+      //addCredentialIdToOptions("allowCredentialsIdGetMenu", createCredentialResponse.id)
+    })
+
+    .catch(error => {
+      console.log("error:" + error);
+      webAuthnResponseDiv.textContent = error;
+      //writeHistory("create", webAuthnRequest, error, updatedUrl)
+      throw error;
+    })
+
+}
+
+function writeHistory(historyType, request, response, url) {
+  historyArea = document.getElementById("historyArea")
+  historyArea.textContent =
+    "\n-------"
+    + historyType
+    + " Request--" + Date() + "------------------------\n"
+    + "url:\n "
+    + url
+    + "\n\nrequest:\n "
+    + JSON.stringify(request)
+    + "\n\nresponse:\n "
+    + response
+    + "\n\n---------------------------------\n"
+    + historyArea.value;
+}
+
+function createCredential(request) {
+  return navigator.credentials.create({
+    publicKey: decodePublicKeyCredentialCreationOptions(request)
+  });
+}
+
+function decodePublicKeyCredentialCreationOptions(request) {
+  const excludeCredentials = request.excludeCredentials && request.excludeCredentials.map(credential => extend(
+    credential, {
+    id: toByteArray(credential.id),
+  }));
+
+  const publicKeyCredentialCreationOptions = extend(
+    request, {
+    attestation: request.attestation,
+    user: extend(
+      request.user, {
+      id: toByteArray(request.user.id),
+    }),
+    challenge: toByteArray(request.challenge),
+    excludeCredentials
+  });
+
+  var webAuthnRequestDiv = document.getElementById("webauthCreateRequest");
+
+  //webAuthnRequestDiv.textContent = JSON.stringify(publicKeyCredentialCreationOptions);
+
+  return publicKeyCredentialCreationOptions;
+}
+
+
+
+
+
+//--------------------------------------------------------------
+function decodeAttestationObj(attestationObject) {
+  return attestationObjectDecoded;
+}
+
+function extend(obj, more) {
+  return Object.assign({}, obj, more);
+}
+
+function toByteArray(code) {
+  return base64js.toByteArray(base64UrlToMime(code));
+}
+
+function base64UrlToMime(code) {
+  return code.replace(/-/g, '+').replace(/_/g, '/') + '===='.substring(0, (4 - (code.length % 4)) % 4);
+}
+
+function responseToObject(response) {
+  if (response.u2fResponse) {
+    return response;
+  } else {
+    let clientExtensionResults = {};
+
+    try {
+      clientExtensionResults = response.getClientExtensionResults();
+    } catch (e) {
+      console.error('getClientExtensionResults failed', e);
+    }
+
+    if (response.response.attestationObject) {
+      return {
+        type: response.type,
+        id: response.id,
+        response: {
+          attestationObject: fromByteArray(response.response.attestationObject),
+          clientDataJSON: fromByteArray(response.response.clientDataJSON),
+        },
+        clientExtensionResults,
+      };
+    } else {
+      return {
+        type: response.type,
+        id: response.id,
+        response: {
+          authenticatorData: fromByteArray(response.response.authenticatorData),
+          clientDataJSON: fromByteArray(response.response.clientDataJSON),
+          signature: fromByteArray(response.response.signature),
+          userHandle: response.response.userHandle && fromByteArray(response.response.userHandle),
+        },
+        clientExtensionResults,
+      };
+    }
+  }
+}
+
+function mimeBase64ToUrl(code) {
+  return code.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+}
+
+function fromByteArray(bytes) {
+  return mimeBase64ToUrl(base64js.fromByteArray(ensureUint8Array(bytes)));
+}
+
+function ensureUint8Array(arg) {
+  if (arg instanceof ArrayBuffer) {
+    return new Uint8Array(arg);
+  } else {
+    return arg;
+  }
+}
